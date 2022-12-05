9253c31d8ed443b3c74ccfc07b13c370692c667e
UniK <unik@users.noreply.github.com>
2022-12-05 23:16:57 +0100

    implement part two of day three

--- Day03.java
+++ Day03.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.lang.System.out;
@@ -26,12 +27,30 @@ public class Day03 {
                 .map(Day03::calcValue)
                 .toList();
 
+        int BATCH_SIZE = 3;
+        List<Integer> valueOfCommonChars = IntStream.iterate(
+                        0,
+                        i -> i < stringList.size(),
+                        i -> i + BATCH_SIZE)
+                // extract the triple (sublist with tree elements) from the initial list
+                .mapToObj(i -> stringList.subList(i, Math.min(i + BATCH_SIZE, stringList.size())))
+                // find common character
+                .map(Day03::findFirstCommonCharacterInThreeLines)
+                .map(Day03::calcValue)
+                .toList();
+
         /* results */
         int sum = integers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
 
         out.println("Part One Answer: " + sum);
+
+        int sumPartTwo = valueOfCommonChars.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        out.println("Part Two Answer: " + sumPartTwo);
     }
 
     private static Character findFirstCommonCharacter(String line) {
@@ -50,6 +69,18 @@ public class Day03 {
                 .toList();
 
         return collect.get(0);
+    }
+
+    private static Character findFirstCommonCharacterInThreeLines(List<String> triple) {
+        Character commonCharacter = triple.get(0).codePoints()
+                .mapToObj(c -> (char) c)
+                .filter(c ->
+                        triple.get(1).indexOf(c) >= 0
+                     && triple.get(2).indexOf(c) >= 0)
+                .findAny()
+                .orElse(null);
+
+        return commonCharacter;
     }
 
     /* value */
